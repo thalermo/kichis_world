2f6098fd5609fdfabf8293e78f50252c8b65017b
Moran <thaler.moran@gmail.com>
2022-04-13 14:41:18 +0200

    fixed kichis position

--- src/components/Kichi.jsx
+++ src/components/Kichi.jsx
@@ -1,9 +1,10 @@
 import kichi from "../images/kichi.webp";
+import "./Kichi.css";
 const Kichi = (props) => (
-    // wrap it in a div so I can add the onClick attribute
-    <div onClick={props.action}>
-      <img src={kichi} alt="Kichi" className="App-logo" />
-    </div>
-  );
-  
-  export default Kichi;
+  // wrap it in a div so I can add the onClick attribute
+  <div onClick={props.action}>
+    <img src={kichi} alt="Kichi" className="figure" />
+  </div>
+);
+
+export default Kichi;
