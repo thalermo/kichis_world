5307789c1c490194e5d3bb218fe221e63a178a06
Moran <thaler.moran@gmail.com>
2022-04-14 16:34:45 +0200

    added the buttons, and tart to place them with a grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import welcome from "./components/bubble_texts/welcome_text";
 import Bubble from "./components/Bubble";
-import Kichi from "./components/Kichi"; 
+import Kichi from "./components/Kichi";
+import { StartButton, PlusButton } from "./components/Button";
 
 // import "./index.css";
 
@@ -12,7 +13,10 @@ function App() {
         {welcome.map((element) => (
           <Bubble key={element.key} text={element.phrase} />
         ))}
-        <Kichi/>
+        <Kichi />
+        <div>
+          <PlusButton />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import "./Button.css";
+
+export const PlusButton = () => {
+  return (
+    <div className="plus-minus--buttons ">
+      <div className="video-game-button plus-grid">+</div>
+      {/* <div className="video-game-button minus-grid">-</div> */}
+    </div>
+  );
+};
+
+export const MinusButton = () => {
+  return;
+};
+
+export const SelectButton = () => {
+  return <span className="start-btn">SELECT</span>;
+};
+
+export const StartButton = () => {
+  return <span className="start-btn">START</span>;
+};
